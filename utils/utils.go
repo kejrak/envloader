22cc338d5884808e03cb5af3c9ae51a7e95fa87d
Jan Kejř <jan.kejr@centrum.cz>
2023-12-18 15:30:10 +0100

    Add GetEnv function for define script runtime

--- utils/utils.go
+++ utils/utils.go
@@ -61,3 +61,11 @@ func WrapBytes(bytes []byte) string {
 
 	return result
 }
+
+func GetEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
